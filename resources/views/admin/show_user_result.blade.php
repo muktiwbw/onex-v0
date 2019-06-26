3b65386971a82c6f3dc08b261a16b282c3be0af4
Putrawitama <putrawitama@gmail.com>
2019-06-27 01:16:07 +0700

    user result

--- resources/views/admin/show_user_result.blade.php
+++ resources/views/admin/show_user_result.blade.php
@@ -16,4 +16,65 @@
         <p>Verified</p>
         @endif
     </div>
+
+    <h1 class="h3 mb-4 text-gray-800">
+        {{$answer_sheet->level->name}} - Hasil
+    </h1>
+    <h4 class="h3 mb-4 text-gray-800 text-center">
+        {{$answer_sheet->user->name}}
+    </h4>
+    <div class="row">
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Mulai Pengerjaan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$answer_sheet->created_at}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Total (Multiple choice & Checklist)</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $answer_sheet->report()->count() == 0 ? $total_score : 'Belum Ada Nilai'}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-check fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Penilaian Diri</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $answer_sheet->report()->count() > 0 ? $answer_sheet->evaluation_answers()->where('answer', true)->count() : 'Belum Ada Nilai'}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-star fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="text-center">
+        <a href="{{route('user')}}" class="btn btn-primary">Kembali ke Beranda</a>
+        <a href="{{route('user-exam-reset', ['level_id' => $answer_sheet->level->id])}}" class="btn btn-success">Remedial</a>
+    </div>
 @endsection
