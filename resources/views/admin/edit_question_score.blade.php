62af6cdf03ce6b56673f34e69f0fd7e003ba8379
Putrawitama <putrawitama@gmail.com>
2019-06-27 01:58:26 +0700

    fixing

--- resources/views/admin/edit_question_score.blade.php
+++ resources/views/admin/edit_question_score.blade.php
@@ -1,36 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$level->name}} - Edit Skor Soal</title>
-</head>
-<body>
-    @component('components.navbar')@endcomponent
-    <h1>Level {{$level->name}}</h1>
-    <form action="{{route('admin-question-score-patch')}}" method="post">
-        @if($level->case_studies()->has('questions')->count() > 0)
-        <div id="cs-question">
-            @foreach($level->case_studies()->has('questions')->orderBy('number', 'asc')->get() as $cs)
-            <h2><a href="{{route('admin-case-study', ['case_study_id' => $cs->id])}}">Studi Kasus {{$cs->number}}: {{$cs->title}}</a></h2>
-                @foreach($cs->questions()->orderBy('number', 'asc')->get() as $question)
-                <p>{{$question->number}}. {{$question->answer_type}} <input type="number" name="question[]" value="{{$question->score}}"></p>
-                @endforeach
-            @endforeach    
+@extends('admin.main')
+@section('content')
+    <h1 class="h3 mb-4 text-gray-800">
+        Edit Skor Soal
+    </h1>
+    <div class="row">
+        <div class="col-6">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">
+                        Level {{$level->name}}
+                    </h6>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{route('admin-question-score-patch')}}" method="post">
+                        @if($level->case_studies()->has('questions')->count() > 0)
+                            <div id="cs-question">
+                                @foreach($level->case_studies()->has('questions')->orderBy('number', 'asc')->get() as $cs)
+                                <h2><a href="{{route('admin-case-study', ['case_study_id' => $cs->id])}}">Studi Kasus {{$cs->number}}: {{$cs->title}}</a></h2>
+                                    @foreach($cs->questions()->orderBy('number', 'asc')->get() as $question)
+                                    <div class="form-group" tail="true" eval-number=1>
+                                        <label>{{$question->number}}. {{$question->answer_type}}</label>
+                                        <input class="form-control" type="number" name="question[]" value="{{$question->score}}">
+                                    </div> 
+                                    @endforeach
+                                @endforeach    
+                            </div>
+                        @endif
+                        @if($level->questions()->doesnthave('case_study')->count() > 0)
+                            <div id="question">
+                                @foreach($level->questions()->doesnthave('case_study')->orderBy('number', 'asc')->get() as $question)
+                                <div class="form-group" tail="true" eval-number=1>
+                                    <label>{{$question->number}}. {{$question->answer_type}}</label>
+                                    <input class="form-control" type="number" name="question[]" value="{{$question->score}}">
+                                </div>
+                                @endforeach
+                            </div>
+                        @endif
+                        <p>*) Skor tipe soal checklist merupakan skor individual tiap poin checklist</p>
+                        <input type="hidden" name="level_id" value="{{$level->id}}">
+                        <input type="submit" class="btn btn-primary" value="Simpan">
+                        @csrf
+                    </form>
+                </div>
+            </div>
         </div>
-        @endif
-        @if($level->questions()->doesnthave('case_study')->count() > 0)
-        <div id="question">
-            @foreach($level->questions()->doesnthave('case_study')->orderBy('number', 'asc')->get() as $question)
-            <p>{{$question->number}}. {{$question->answer_type}} <input type="number" name="question[]" value="{{$question->score}}"></p>
-            @endforeach
-        </div>
-        @endif
-        <p>*) Skor tipe soal checklist merupakan skor individual tiap poin checklist</p>
-        <input type="hidden" name="level_id" value="{{$level->id}}">
-        <input type="submit" value="Submit">
-        @csrf
-    </form>
-</body>
-</html>
+    </div>
+@endsection
--- resources/views/admin/show_user_result.blade.php
+++ resources/views/admin/show_user_result.blade.php
@@ -1,22 +1,5 @@
 @extends('admin.main')
-@section('content')  
-    <h2>{{$answer_sheet->level->name}} - Hasil</h2>
-    <div id="profile">
-        <p>Nama: {{$answer_sheet->user->name}}</p>
-        <p>Mulai: {{$answer_sheet->created_at}}</p>
-        @if($answer_sheet->report()->count() == 0)
-        <p>Skor Total (Multiple choice & Checklist): {{$total_score}}</p>
-        @endif
-        @if($answer_sheet->report()->count() == 0 && $answer_sheet->level->questions()->where('answer_type', 'ESSAY')->count() > 0)
-        <p><a href="{{route('admin-user-essay-check', ['user_id' => $answer_sheet->user_id, 'level_id' => $answer_sheet->level_id])}}">Check Essay</a></p>
-        @elseif($answer_sheet->report()->count() == 0 && $answer_sheet->level->questions()->where('answer_type', 'ESSAY')->count() == 0)
-        <p><a href="#">Verify</a></p>
-        @else
-        <p>Skor Total: {{$answer_sheet->report->score}}</p>
-        <p>Verified</p>
-        @endif
-    </div>
-
+@section('content')
     <h1 class="h3 mb-4 text-gray-800">
         {{$answer_sheet->level->name}} - Hasil
     </h1>
@@ -39,30 +22,38 @@
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-danger shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Total (Multiple choice & Checklist)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $answer_sheet->report()->count() == 0 ? $total_score : 'Belum Ada Nilai'}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-check fa-2x text-gray-300"></i>
+        @if($answer_sheet->report()->count() == 0)
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Total (Multiple choice & Checklist)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{  $total_score }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
+
 
         <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Penilaian Diri</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $answer_sheet->report()->count() > 0 ? $answer_sheet->evaluation_answers()->where('answer', true)->count() : 'Belum Ada Nilai'}}</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Skor Total</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                @if($answer_sheet->report()->count() == 0 && $answer_sheet->level->questions()->where('answer_type', 'ESSAY')->count() > 0)
+                                    <a class="btn btn-success mt-2" href="{{route('admin-user-essay-check', ['user_id' => $answer_sheet->user_id, 'level_id' => $answer_sheet->level_id])}}">Check Essay</a>
+                                @else
+                                    {{$answer_sheet->report->score}}
+                                @endif
+                            </div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-star fa-2x text-gray-300"></i>
@@ -71,10 +62,24 @@
                 </div>
             </div>
         </div>
-    </div>
-    
-    <div class="text-center">
-        <a href="{{route('user')}}" class="btn btn-primary">Kembali ke Beranda</a>
-        <a href="{{route('user-exam-reset', ['level_id' => $answer_sheet->level->id])}}" class="btn btn-success">Remedial</a>
+
+        @if(!($answer_sheet->report()->count() == 0 && $answer_sheet->level->questions()->where('answer_type', 'ESSAY')->count() > 0))
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Status</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Verified</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
     </div>
 @endsection
